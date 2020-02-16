d30233cc5960a41694036c4268accdbcc96b6231
tjh <1937471723@qq.com>
2020-02-16 15:39:17 +0800

    添加账号设置中上传头像功能

---
+++ src/main/java/com/tjh/newcoder/controller/UserController.java
@@ -0,0 +1,107 @@
+package com.tjh.newcoder.controller;
+
+
+import com.tjh.newcoder.dao.UserMapper;
+import com.tjh.newcoder.entity.User;
+import com.tjh.newcoder.util.CommunityUtil;
+import com.tjh.newcoder.util.HostHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
+    @Value("${blog.path.upload}")
+    private String uploadPath;
+
+    @Value("${blog.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+
+    @Autowired
+    private HostHolder hostHolder;
+    @Autowired
+    private UserMapper userMapper;
+
+
+    //设置页面
+    @RequestMapping("/setting")
+    public String getSettingPage() {
+        return "/site/setting";
+    }
+
+    //上传头像
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String upload(MultipartFile headerImage, Model model) {
+        if (headerImage == null) {
+            model.addAttribute("error", "您还没有选择图片");
+            return "site/setting";
+        }
+        //MultipartFile表示HTML中以表单形式上传的文件（包含二进制数据和文件名称）
+        //获取上传文件的后缀名
+        String fileName = headerImage.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if (StringUtils.isBlank(suffix)) {
+            model.addAttribute("error", "文件格式错误");
+            return "/site/setting";
+        }
+        //生成随机文件名（防止重复）
+        fileName = CommunityUtil.generateUUID() + suffix;
+        //设置文件在服务器本地保存的路径
+        File dest = new File(uploadPath + "/" + fileName);
+        //将用户选择的文件上传到服务器指定路径下
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败: " + e.getMessage());
+            throw new RuntimeException("上传文件失败,服务器发生异常!", e);
+        }
+        //更新用户头像路径（一定是WEB访问路径）
+        // http://localhost:8080/tjhblog/user/header/xxx.png
+        User user = hostHolder.getUser();
+        String url = domain + contextPath + "/user/header/" + fileName;
+        userMapper.updateHeader(user.getId(), url);
+
+        return "redirect:/index";
+    }
+
+
+    //获取头像的WEB访问路径(相应图片逻辑返回值为空，使用response处理)
+    @RequestMapping("/header/{filename}")
+    public void getHeader(HttpServletResponse response, @PathVariable("filename") String filename) {
+        //服务器存放路径
+        filename = uploadPath + "/" + filename;
+        //获取文件后缀
+        String suffix = filename.substring(filename.lastIndexOf("."));
+        System.out.println(suffix);
+        //相应指定格式图片
+        response.setContentType("image/" + suffix);
+        //文件流需要关闭 这样写自动关闭
+        try (FileInputStream fis = new FileInputStream(filename);
+             OutputStream os = response.getOutputStream();) {
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while ((b = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败: " + e.getMessage());
+        }
+    }
+}
